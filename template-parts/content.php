63962c40a4257a079967307095eff8048de989bd
irfatka <37922941+irfatka@users.noreply.github.com>
2018-05-07 10:55:55 +0530

    Add files via upload

---
+++ template-parts/content.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Elegant_Magazine
+ */
+
+?>
+    <?php if (is_singular()) : ?>
+            <div class="entry-content">
+                <?php
+                the_content(sprintf(
+                    wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'elegant-magazine'),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    get_the_title()
+                ));
+
+                wp_link_pages(array(
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'elegant-magazine'),
+                    'after' => '</div>',
+                ));
+                ?>
+            </div><!-- .entry-content -->
+            <footer class="entry-footer">
+                <?php //elegant_magazine_entry_footer(); ?>
+            </footer>
+
+
+    <?php else: ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('col col-three-point-three'); ?>>
+            <?php elegant_magazine_page_layout_blocks(); ?>
+        <footer class="entry-footer">
+            <?php
+                wp_link_pages(array(
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'elegant-magazine'),
+                    'after' => '</div>',
+                ));
+            ?>
+        </footer>
+    </article>
+    <?php endif; ?>
+
+
